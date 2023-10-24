149e138d0b762f368e90c548bdbbab97504d536f
Pingxin Gao <gaopingxin0315@gmail.com>
2023-10-24 17:33:49 +0300

    update load_env location

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env
 
 class Config:
-    SECRET_KEY = "123123123213"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+mysqldb://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@"
         f"{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}?charset=utf8mb4"
