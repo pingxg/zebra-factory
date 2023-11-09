81d71c5b6ea28713ff25787721793899c0ff09f2
Pingxin Gao <gaopingxin0315@gmail.com>
2023-11-09 16:53:31 +0200

    update the delivery note to check if the product it salmon

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 import os
 import time
+import threading
 
 import socketio
 import logging
 from print_utils import pdf_render_print
+
 
 # Set up logging with timestamp
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -20,6 +22,8 @@ def on_connect():
 def on_disconnect():
     """Callback for when the client disconnects from the server."""
     logger.info("Disconnected from the server.")
+
+
 
 @sio.on('print_zebra')
 def on_print(data):
@@ -70,7 +74,6 @@ def main():
         sio.connect(link)
         
         # Start the keepalive loop in a separate thread
-        import threading
         keepalive_thread = threading.Thread(target=keepalive_loop)
         keepalive_thread.start()
 
@@ -80,3 +83,66 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# @sio.event
+# def connect():
+#     logger.info("Connected to the server.")
+
+# @sio.event
+# def disconnect():
+#     logger.info("Disconnected from the server.")
+
+# @sio.event
+# def print_zebra(data):
+#     process_print_event(data, "zpl")
+
+# @sio.event
+# def print_pdf(data):
+#     process_print_event(data, "pdf")
+
+# def process_print_event(data, file_type):
+#     order_id = data.get('order_id')
+#     if order_id:
+#         logger.info(f"Received order ID: {order_id}. Printing {file_type.upper()} label.")
+#         try:
+#             pdf_render_print(order_id, file_type=file_type)
+#         except Exception as e:
+#             logger.error(f"Error while processing order ID {order_id}: {e}")
+#     else:
+#         logger.warning(f"Received 'print_{file_type}' event without order ID.")
+
+# @sio.event
+# def keepalive_response():
+#     logger.info("Received keepalive response from server.")
+
+# def keepalive_loop(stop_event):
+#     while not stop_event.is_set():
+#         time.sleep(10)
+#         sio.emit('keepalive', {})
+#         logger.debug("Keepalive message sent.")
+
+# def main():
+#     link = os.environ.get('LINK')
+#     if not link:
+#         logger.error("Environment variable 'LINK' not set. Please set the 'LINK' environment variable with the server URL.")
+#         return
+
+#     try:
+#         sio.connect(link)
+
+#         stop_event = threading.Event()
+#         keepalive_thread = threading.Thread(target=keepalive_loop, args=(stop_event,))
+#         keepalive_thread.start()
+
+#         sio.wait()
+#     except Exception as e:
+#         logger.error(f"Error connecting to server: {e}")
+#     finally:
+#         stop_event.set()
+#         keepalive_thread.join()
+
+# if __name__ == "__main__":
+#     main()
