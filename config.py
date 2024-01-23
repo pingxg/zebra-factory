45c05d359d6407307cc67b58dd7862fe8d121f5f
Pingxin Gao <gaopingxin0315@gmail.com>
2024-01-23 10:31:54 +0200

    remove the restrictions for adding the weight

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    # Configures database and session settings.
+    # - SQLALCHEMY_DATABASE_URI: Connection string for database. Uses environment variables for credentials and host details.
+    # - SQLALCHEMY_POOL_SIZE: Number of connections to pool in database connection pool. 
+    # - SQLALCHEMY_POOL_TIMEOUT: Time to wait before releasing idle connections in pool.
+    # - SQLALCHEMY_TRACK_MODIFICATIONS: Whether to track modifications on ORM models.
+    # - REMEMBER_COOKIE_DURATION: Lifetime of remember me cookies.
+    # - SECRET_KEY: Secret key for cryptographic functions like signing cookies.
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+mysqldb://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@"
         f"{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}?charset=utf8mb4"
--- generate_sql.py
+++ generate_sql.py
@@ -29,7 +29,7 @@ import csv
 import os
 
 # Path to your CSV file
-csv_file_path = 'vk03.csv'
+csv_file_path = 'vk04.csv'
 
 # Start of the SQL statement
 sql = "INSERT INTO data.salmon_orders (date, product, customer, quantity, price) VALUES "
