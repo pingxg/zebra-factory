b54d9da646d409d9dd78b7890d38a2161f8cd2a4
Pingxin Gao <gaopingxin0315@gmail.com>
2024-05-06 22:27:21 +0300

    update main page style

--- app/blueprints/print/views.py
+++ app/blueprints/print/views.py
@@ -14,12 +14,3 @@ def emit_print_zebra():
     flash('Print zebra event emitted','success')
     return jsonify({'status': 'Print zebra event emitted'})
 
-# Routes
-@print_bp.route('/emit_print_pdf', methods=['POST'])
-@roles_required('admin', 'driver', 'cutter')
-def emit_print_pdf():
-    data = request.json
-    order_id = data.get('order_id')
-    socketio.emit('print_pdf', {'order_id': order_id})
-    flash('Print pdf event emitted','success')
-    return jsonify({'status': 'Print pdf event emitted'})
